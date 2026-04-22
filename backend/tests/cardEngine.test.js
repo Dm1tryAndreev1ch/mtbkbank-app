66c70136bf8fca19ce69d7312fea3f707657dcaa
Andrew S4pph1re <andreygoncharenok18@gmail.com>
2026-04-23 01:27:18 +0300

    fixed mockPrisma

--- backend/tests/cardEngine.test.js
+++ backend/tests/cardEngine.test.js
@@ -15,6 +15,7 @@ const mockPrisma = {
   notification: { create: jest.fn() },
   transaction: { update: jest.fn() },
   deckCard: { deleteMany: jest.fn() },
+  $executeRaw: jest.fn(),
   $transaction: jest.fn(async (fn) => fn(mockPrisma)),
 };
 
